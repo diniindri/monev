44a78288724aa8c86527df12f510e8f2e15e44d6
Alam Firdaus <alamfirdaus@Alams-MacBook-Pro.local>
2021-07-29 14:59:46 +0700

    perubahan kedua

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -2,16 +2,6 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
     </div>
-    <div class="row mb-3">
-        <div class="col">
-            <div class="alert alert-success" role="alert">
-                <h4 class="alert-heading">Well done!</h4>
-                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-                <hr>
-                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-            </div>
-        </div>
-    </div>
 
     <div class="row mb-3">
         <div class="col-lg-8">
---
+++ application/views/verifikasi/index.php
@@ -0,0 +1,74 @@
+<main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Verifikasi</h1>
+    </div>
+    <div class="row">
+        <div class="col">
+            <?php if ($this->session->flashdata('pesan')) : ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Selamat!</strong> <?= $this->session->flashdata('pesan'); ?>
+                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-lg-7">
+        </div>
+        <div class="col-lg-5">
+            <form action="" method="post" autocomplete="off">
+                <div class="input-group">
+                    <input type="text" name="nomor" class="form-control" placeholder="nomor SPP/SPBy">
+                    <button class="btn btn-sm btn-outline-secondary" type="submit">Cari</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead class="text-center">
+                        <tr class="align-middle">
+                            <th>No</th>
+                            <th>Nomor SPM</th>
+                            <th>Tanggal SPM</th>
+                            <th>Nomor SP2D</th>
+                            <th>Tanggal SP2D</th>
+                            <th>Jumlah Bruto</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = $page + 1;
+                        foreach ($tagihan as $r) : ?>
+                            <tr>
+                                <td class="text-center"><?= $no++; ?></td>
+                                <td><?= $r['']; ?></td>
+                                <td><?= $r['nomor']; ?></td>
+                                <td><?= $r['tanggal']; ?></td>
+                                <td><?= $r['uraian']; ?></td>
+                                <td><?= $r['detail']; ?></td>
+                                <td><?= $r['bruto']; ?></td>
+                                <td class="pb-0">
+                                    <div class="btn-group btn-group-sm" role="group">
+                                        <a href="<?= base_url('verifikasi/setuju/') ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0" onclick="return confirm('Apakah Anda yakin setuju dengan data ini?');">Setuju</a>
+                                        <a href="<?= base_url('verifikasi/tolak/') ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0" onclick="return confirm('Apakah Anda yakin akan menolak data ini?');">Tolak</a>
+                                        <a href="<?= base_url('data-tagihan/upload/') ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0">Upload</a>
+                                        <a href="<?= base_url('data-tagihan/kirim/') ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0">Kirim</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <?= $nomor == null ? $pagination : ''; ?>
+        </div>
+    </div>
+
+</main>
