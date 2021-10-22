8ef5037df57fc62bf1d34eaa9f725e0f20c26ca9
Alam Firdaus <alamfirdaus@Alams-MacBook-Pro.local>
2021-10-22 14:32:21 +0700

    menu referensi pagu

---
+++ application/models/Ref_pagu_model.php
@@ -0,0 +1,55 @@
+<?php
+
+class Ref_pagu_model extends CI_Model
+{
+    public function getPagu($limit = 0, $offset = 0)
+    {
+        $this->db->limit($limit, $offset);
+        return $this->db->get('ref_pagu')->result_array();
+    }
+
+    public function getDetailPagu($id = null)
+    {
+        return $this->db->get_where('ref_pagu', ['id' => $id])->row_array();
+    }
+
+    public function findPagu($kro = null, $limit = 0, $offset = 0)
+    {
+        $this->db->limit($limit, $offset);
+        return $this->db->get_where('ref_pagu', ['kro' => $kro])->result_array();
+    }
+
+    public function countPagu()
+    {
+        return $this->db->get('ref_pagu')->num_rows();
+    }
+
+    public function createPagu($data = null)
+    {
+        $this->db->insert('ref_pagu', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function updatePagu($data = null, $id = null)
+    {
+        $this->db->update('ref_pagu', $data, ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function deletePagu($id = null)
+    {
+        $this->db->delete('ref_pagu', ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function getAllPagu()
+    {
+        $this->db->order_by('kegiatan', 'ASC');
+        return $this->db->get('ref_pagu')->result_array();
+    }
+
+    public function findJumlahPagu($kro = null, $ro = null)
+    {
+        return $this->db->get_where('ref_pagu', ['kro' => $kro, 'ro' => $ro])->row_array();
+    }
+}
