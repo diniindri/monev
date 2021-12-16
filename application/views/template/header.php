955b8b872c30fc1d7804de1f19a487df867189ba
danakristiawan <danakristiawan@gmail.com>
2021-12-16 12:53:46 +0700

    menambahkan lambang

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <title>Monev</title>
-    <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/alika.png" type=" image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/monev.png" type=" image/x-icon">
     <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?= base_url(); ?>assets/css/dashboard.css" rel="stylesheet">
 </head>
@@ -14,7 +14,7 @@
 <body>
 
     <header class="navbar navbar-dark sticky-top bg-info flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="<?= base_url('tahun-anggaran'); ?>" style="font-size: 20px;"><img src="<?= base_url(); ?>assets/img/alika.png" width="25" height="25"> &nbsp;Monev <?= $this->session->userdata('tahun'); ?></a>
+        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="<?= base_url('tahun-anggaran'); ?>" style="font-size: 20px;"><img src="<?= base_url(); ?>assets/img/monev.png" width="25" height="25"> &nbsp;Monev <?= $this->session->userdata('tahun'); ?></a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
